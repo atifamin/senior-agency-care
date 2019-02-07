27575c8c4114b8bb817151ee36cf42dd4eabcae4
addi009 <addi009@users.noreply.github.com>
2019-02-07 19:24:08 +0500

    commit

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,5 +50,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'login';
+/*$route['caregiver/login'] = 'caregiver/login/index';
+$route['caregiver'] = 'caregiver/caregiver/index';
+$route['caregiver/(:any)'] = 'caregiver/caregiver/$1';*/
+
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
