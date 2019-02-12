df0eb3e3e2da8d99a7f31f0929c728f203b4efa8
usamaajaved <46658189+usamaajaved@users.noreply.github.com>
2019-02-12 19:10:00 +0500

    client dietry needs

--- application/views/agency/scheduling/index.php
+++ application/views/agency/scheduling/index.php
@@ -3,6 +3,10 @@
 	.navbar-nav-link.active{
 		background-color: #fff !important;
 	}
+
+	.token-input{
+		min-width: 100% !important;
+	}
 </style>
 
 <div class="row">
@@ -167,7 +171,62 @@
 									medication_list
 								</div>
 								<div class="tab-pane fade active" id="schedule_dietry_needs_1">
-									dietry_needs
+									<form>
+										<div class="row">
+											<div class="col-md-6">
+												<div class="form-group pt-2">
+													<label class="d-block font-weight-semibold">Select client's dietry requirements</label>
+													<div class="custom-control custom-radio">
+														<input type="radio" class="custom-control-input" name="client_dietry_requirements" id="regular_diet" checked="">
+														<label class="custom-control-label" for="regular_diet">Regular diet</label>
+													</div>
+
+													<div class="custom-control custom-radio">
+														<input type="radio" class="custom-control-input" name="client_dietry_requirements" id="mechanical_diet">
+														<label class="custom-control-label" for="mechanical_diet">Mechanical diet</label>
+													</div>
+
+													<div class="custom-control custom-radio">
+														<input type="radio" class="custom-control-input" name="client_dietry_requirements" id="pure_diet">
+														<label class="custom-control-label" for="pure_diet">Puree diet</label>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-6">
+												<div class="form-group pt-2">
+													<label class="d-block font-weight-semibold">Select client's dietry requirements</label>
+													<div class="custom-control custom-radio">
+														<input type="radio" class="custom-control-input" name="client_fluids_requirements" id="thin_liquids" checked="">
+														<label class="custom-control-label" for="thin_liquids">Thin liquids</label>
+													</div>
+
+													<div class="custom-control custom-radio">
+														<input type="radio" class="custom-control-input" name="client_fluids_requirements" id="honey_thick_liquids">
+														<label class="custom-control-label" for="honey_thick_liquids">Honey thick liquids</label>
+													</div>
+
+													<div class="custom-control custom-radio">
+														<input type="radio" class="custom-control-input" name="client_fluids_requirements" id="nector_thick_liquids">
+														<label class="custom-control-label" for="nector_thick_liquids">Nector thick liquids</label>
+													</div>
+												</div>
+											</div>
+										</div>
+
+										<div class="row">
+											<div class="col-md-6">
+												<div class="form-group">
+													<label class="d-block font-weight-semibold">List client's known allergies</label>
+													<input style="width: 100%;" type="text" class="form-control tokenfield" value="" placeholder="Add multiple client's allergies(with comma seperated)" data-fouc>
+												</div>
+											</div>
+										</div>
+
+										<div style = "position:relative; left: 900px; top:2px;"><button type="button" class="btn btn-primary legitRipple">Save<i style="margin-left: 8px;" class="icon-arrow-right14"></i></button>
+										</div>
+
+									</form>
+
 								</div>
 								<div class="tab-pane fade active" id="schedule_appiontment_callender_1">
 									appiontment_callender
@@ -347,6 +406,9 @@
 	<script src="<?php echo base_url(); ?>/assets/js/plugins/ui/moment/moment.min.js"></script>
 	<script src="<?php echo base_url(); ?>/assets/js/plugins/ui/fullcalendar/fullcalendar.min.js"></script>
 	<script src="<?php echo base_url(); ?>/assets/js/demo_pages/fullcalendar_basic.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/plugins/forms/tags/tagsinput.min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/plugins/forms/tags/tokenfield.min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/demo_pages/form_tags_input.js"></script>
 	<!-- /theme JS files -->
 <?php include(APPPATH."views/agency/inc/footer.php");?>
 
