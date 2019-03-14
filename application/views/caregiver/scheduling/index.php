0eadb3c6ac3d94390add1993e0109fc49e786624
usamaajaved <46658189+usamaajaved@users.noreply.github.com>
2019-03-14 18:44:50 +0500

    caregiver schedule

--- application/views/caregiver/scheduling/index.php
+++ application/views/caregiver/scheduling/index.php
@@ -1,3 +1,18 @@
 <?php include(APPPATH."views/caregiver/inc/header.php"); ?>
-Scheduling
+<div class="row" style="width: 100%; margin-left: 1px;">
+  <div class="col-md-12" style="padding: 0;"> 
+  	<div class="card card-body">
+  		<!-- List view -->
+    	<div class="fullcalendar-list"></div>
+    	<!-- /list view -->
+  	</div> 
+  </div>
+</div>
+
+
+<script src="<?php echo base_url(); ?>assets/js/plugins/ui/moment/moment.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/plugins/ui/fullcalendar/fullcalendar.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/demo_pages/fullcalendar_basic.js"></script>
+
+
 <?php include(APPPATH."views/caregiver/inc/footer.php"); ?>
