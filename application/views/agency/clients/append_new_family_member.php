99cc2608d4898fcb0fc404d57345e0ae0d43b910
zubair042 <042zubair@github.com>
2019-03-22 13:21:44 +0500

    commit

--- application/views/agency/clients/append_new_family_member.php
+++ application/views/agency/clients/append_new_family_member.php
@@ -1,5 +1,5 @@
 
-<div class="row" style="padding: 15px 0px;">
+<div class="row" style="padding: 15px 0px;" id="family_member_row_<?php echo $family_id; ?>">
 	<div class="col-md-1 offset-md-2">
 		<!-- <input type="hidden" id="edit_counter" value=""> -->
 	    <img src="<?php echo base_url(); ?>assets/images/userimg/face5.jpg" class="rounded-circle" width="40" height="40" alt="">
@@ -11,6 +11,12 @@
 	    </div>
 	</div>
 	<div class="col-md-3">
-	    <button type="button" onclick="editFamilyMember('<?php echo $family_id; ?>')" class="btn btn-outline alpha-success text-success-800 border-success-600 legitRipple">Edit<i style="font-size: 10px; margin-left: 20px;" class="icon-arrow-down15"></i></button>
+	    <div class="btn-group">
+        	<button type="button" class="btn btn-outline alpha-success text-success-800 border-success-600 legitRipple dropdown-toggle" data-toggle="dropdown">Change</button>
+        	<div class="dropdown-menu dropdown-menu-right">
+				<a href="#" onclick="editFamilyMember('<?php echo $family_id; ?>')" class="dropdown-item"><i class="icon-menu7"></i> Edit</a>
+				<a href="#" onclick="deleteFamilyMember('<?php echo $family_id; ?>')" class="dropdown-item"><i class="icon-database-remove"></i> Delete</a>
+			</div>
+		</div>
 	</div>
 </div>
