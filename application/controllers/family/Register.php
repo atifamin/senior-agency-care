00037cb1a291fec99213d42edc0050762ba9d32c
addi009 <addi009@users.noreply.github.com>
2019-04-29 12:03:09 +0500

    commit

--- application/controllers/family/Register.php
+++ application/controllers/family/Register.php
@@ -3,16 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Register extends CI_Controller {
 
-	public function __construct(){
-		parent::__construct();
-		if(!$this->session->userdata("isFamilyLoggedIn")){
-			redirect("login");
-		}
-		$this->load->model("Family_model");
-		$userSession = $this->session->userdata('isFamilyLoggedIn');
-		$this->family_id = $userSession['user_id'];
-	}
-
 	public function index(){
 		if(!isset($_GET['id'])){
 			redirect("family");
